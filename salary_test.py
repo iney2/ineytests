1239c8267aa55b972ad44dda27bf824ee1ccbc81
iney2 <panov20@gmail.com>
2020-01-31 13:47:09 +1000

    Update salary_test.py

--- salary_test.py
+++ salary_test.py
@@ -6,11 +6,20 @@ uh = urllib.request.urlopen("http://dummy.restapiexample.com/api/v1/employees")
 parsed_string = uh.read()
 json_data = json.loads(parsed_string.decode("utf-8"))
 
-jsonpath_expression = parse('$.data[?(@.employee_name=="Doris Wilder")].employee_salary')
+def emp_information(emp_name, emp_info = 'employee_salary'):    
+ 
+    jsonpath_expression = parse("$.data[?(@.employee_name==" + emp_name + ")]." + emp_info)
 
-for match in jsonpath_expression.find(json_data):
-    print('Doris Wilder salary is', match.value)
-    json_salary = match.value
+    for match in jsonpath_expression.find(json_data):
+        json_salary = match.value
+        
+    return json_salary    
 
-def test_salary():
-    assert json_salary == '85600'
+def test_Wilder_salary():
+    assert emp_information('"Doris Wilder"', 'employee_salary') == '85600'
+    
+def test_Berry_age():
+    assert emp_information('"Yuri Berry"', 'employee_age') == '40'    
+    
+def test_Davidson_ID():
+    assert emp_information('"Rhona Davidson"', 'id') == '8'
